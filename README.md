3f07bf9a5dc1a47502403810460e7cd6fdad01f0
shreydan <iamshreydan@gmail.com>
2022-12-03 12:45:27 +0530

    added cropping toggle

--- README.md
+++ README.md
@@ -8,7 +8,11 @@ torch
 torchvision
 ```
 
-Note: I used centre-crop for the cropping required in the paper.
+Note:
+
+- For cropping as required in the paper, the method I used is center-crop
+- Crop enabled (default) : the skip connections will be cropped as per the upscaled image and the final result will be smaller than the input image.
+- Crop disabled : the upscaled images will be padded as per the skip connections and the final result will be the same size as the input image. 
 
 ## Paper
 
--- unet.py
+++ unet.py
@@ -4,13 +4,14 @@ import torchvision.transforms.functional as vt
 
 
 class UNet(nn.Module):
-    def __init__(self, in_channels: int = 1, num_classes: int = 2) -> None:
+    def __init__(self, in_channels: int = 1, num_classes: int = 2, crop=True) -> None:
 
         super(UNet, self).__init__()
 
         self.channel_list = [64, 128, 256, 512]
         self.in_channels = in_channels
         self.num_classes = num_classes
+        self.crop = crop
 
         self.pooler = nn.MaxPool2d(2)
 
@@ -58,7 +59,10 @@ class UNet(nn.Module):
             x = up_conv(x)
 
             skip = skip_connections[i // 2]
-            skip = vt.center_crop(skip, output_size=x.shape[2:])
+            if self.crop:
+                skip = vt.center_crop(skip, output_size=x.shape[2:])
+            else:
+                x = vt.center_crop(x, output_size=skip.shape[2:])
 
             x = torch.concat([skip, x], dim=1)
 
@@ -73,6 +77,10 @@ class UNet(nn.Module):
 if __name__ == "__main__":
 
     x = torch.rand(1, 1, 572, 572)  # batch x channels x height x width
-    model = UNet(in_channels=1, num_classes=2)
-    print(x.shape)  # expected: B x C x H x W : 1 x 2 x 388 x 388
+    model = UNet(in_channels=1, num_classes=2, crop=False)
+    print(x.shape)
+    """
+      crop enabled => expected: B x C x H x W : 1 x 2 x 388 x 388
+     crop disabled => expected: B x C x H x W : 1 x 2 x 572 x 572
 
+    """
