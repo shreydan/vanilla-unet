916055ccdebdd0033a2fbcbcd809fad19443b82a
shreydan <iamshreydan@gmail.com>
2022-12-04 18:04:26 +0530

    fix output size issue

--- unet.py
+++ unet.py
@@ -34,9 +34,12 @@ class UNet(nn.Module):
         self.output = nn.Conv2d(self.channel_list[0], self.num_classes, 1, 1)
 
     def _double_conv(self, in_channels, out_channels) -> nn.Sequential:
+
+        padding = 0 if self.crop else 1
+
         return nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, 3, 1),
-            nn.Conv2d(out_channels, out_channels, 3, 1),
+            nn.Conv2d(in_channels, out_channels, 3, 1, padding=padding),
+            nn.Conv2d(out_channels, out_channels, 3, 1, padding=padding),
             nn.ReLU(),
         )
 
@@ -59,6 +62,7 @@ class UNet(nn.Module):
             x = up_conv(x)
 
             skip = skip_connections[i // 2]
+
             if self.crop:
                 skip = vt.center_crop(skip, output_size=x.shape[2:])
             else:
@@ -78,7 +82,8 @@ if __name__ == "__main__":
 
     x = torch.rand(1, 1, 572, 572)  # batch x channels x height x width
     model = UNet(in_channels=1, num_classes=2, crop=False)
-    print(x.shape)
+    print(model(x).shape)
+
     """
       crop enabled => expected: B x C x H x W : 1 x 2 x 388 x 388
      crop disabled => expected: B x C x H x W : 1 x 2 x 572 x 572
